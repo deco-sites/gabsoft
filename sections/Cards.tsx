03144c24509d76242246350f81302e1cf6b8021a
Gabriel Almeida <gabriel@agenciaeplus.com.br>
2022-12-27 23:25:41 -0300

    return cards html

--- sections/Cards.tsx
+++ sections/Cards.tsx
@@ -9,17 +9,19 @@ export default function Cards({
   products = [],
 }: Props) {
   return (
-    <>
-      {products.map((product) => {
-        <div class="p-8 bg-[#33cc00]">
-          <h2 class="text-center text-black">{product.title}</h2>
-          <ul class="flex flex-col">
-            {product.list.map((item) => {
-              <li class="text-black">- {item.title}</li>;
-            })}
-          </ul>
-        </div>;
-      })}
-    </>
+    <div class='w-[90%] m-auto flex flex-row mt-[-100px] pb-[200px]'>
+        {products.map((product) => {
+            return (
+          <div class="p-8 bg-[#52bf4d] w-1/3 flex flex-col justify-start items-start mr-[5px] min-h-[252px]">
+            <h2 class="text-center text-black text-xl text-uppercase font-bold mb-[16px]">{product.title}</h2>
+            <ul class="flex flex-col">
+              {product.list.map((item) => {
+                return <li class="text-black text-capitalize text-sm mt-0.5 mb-0.5">- {item.title}</li>
+              })}
+            </ul>
+          </div>
+            )
+        })}
+    </div>
   );
 }
